2dea22541888e5a39950ad8037831f6effd63248
Luke Miller <lukealexmiller@gmail.com>
2017-02-14 16:19:42 +0100

    Added build_run shell script for TF FRCNN

---
+++ tensorflow_faster_rcnn/build_and_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Building Dockerfile"
+sudo "docker build . pool1892/docker:tensorflow_faster_rcnn"
+
+echo "Running Container"
+sudo "nvidia-docker run -it  -p 8888:8888 -p 6006:6006 -v /home/ubuntu/docker/tensorflow_faster_rcnn/output:/root/faster_rcnn/output -v /home/ubuntu/docker/tensorflow_faster_rcnn/experiments/logs:/root/faster_rcnn/experiments/logs"
