968b40742a91d145f1247c5861325faec250a195
Luke Miller <lukealexmiller@gmail.com>
2017-02-14 16:32:34 +0100

    More

--- tensorflow_faster_rcnn/build_and_run.sh
+++ tensorflow_faster_rcnn/build_and_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Building Dockerfile"
-sudo "docker build -t pool1892/docker:tensorflow_faster_rcnn ."
+docker build -t pool1892/docker:tensorflow_faster_rcnn .
 
 echo "Running Container"
-sudo "nvidia-docker run -it  -p 8888:8888 -p 6006:6006 -v /home/ubuntu/docker/tensorflow_faster_rcnn/output:/root/faster_rcnn/output -v /home/ubuntu/docker/tensorflow_faster_rcnn/experiments/logs:/root/faster_rcnn/experiments/logs pool1892/docker:tensorflow_faster_rcnn"
+nvidia-docker run -it  -p 8888:8888 -p 6006:6006 -v /home/ubuntu/docker/tensorflow_faster_rcnn/output:/root/faster_rcnn/output -v /home/ubuntu/docker/tensorflow_faster_rcnn/experiments/logs:/root/faster_rcnn/experiments/logs pool1892/docker:tensorflow_faster_rcnn
