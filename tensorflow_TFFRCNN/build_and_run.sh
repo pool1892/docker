af8410d78cdb74fc91a8469c61035ee026617be9
Luke Miller <lukealexmiller@gmail.com>
2017-03-14 14:22:58 +0100

    Renamed additional FRCNN directory. Added TF 1.0 intermediate.

---
+++ tensorflow_TFFRCNN/build_and_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Building Dockerfile"
+docker build -t pool1892/docker:tensorflow_TFFRCNN .
+
+echo "Running Container"
+nvidia-docker run -it  -p 8888:8888 -p 6006:6006 -v /home/ubuntu/docker/tensorflow_TFFRCNN/output:/root/faster_rcnn/output -v /home/ubuntu/docker/tensorflow_TFFRCNN/experiments/logs:/root/faster_rcnn/experiments/logs pool1892/docker:tensorflow_TFFRCNN
